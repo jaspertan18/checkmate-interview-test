3508aa3b0e09a6c5588c920597a9c380c810b6fd
jasper <sh990818@connect.hku.hk>
2023-02-11 18:45:20 +0800

    Task 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,101 @@
-import Head from 'next/head'
-import GoogleButton from 'react-google-button'
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
-import {useRouter} from "next/router";
-import { initializeApp } from 'firebase/app';
+import Head from "next/head";
+import GoogleButton from "react-google-button";
+import {
+    getAuth,
+    getRedirectResult,
+    GoogleAuthProvider,
+    signInWithRedirect,
+} from "firebase/auth";
+import { useRouter } from "next/router";
+import { initializeApp } from "firebase/app";
 
 // Task 0: Initialize Firebase
 // Replace the following with your app's Firebase project configuration
 // https://firebase.google.com/docs/web/setup
 const firebaseConfig = {
-  // Enter your own firebase config here
+    apiKey: "AIzaSyCt6e_96PzxmHxZQDPhshD_tyAIHcC1FgY",
+    authDomain: "test-5c2b2.firebaseapp.com",
+    projectId: "test-5c2b2",
+    storageBucket: "test-5c2b2.appspot.com",
+    messagingSenderId: "396674885998",
+    appId: "1:396674885998:web:e3e2a5a717d3a2106b397f",
+    measurementId: "G-LEKBXDD1VX",
 };
 
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // GoogleAuthProvider instance
-// const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 // Firebase Auth instance
-// const auth = getAuth(app);
+const auth = getAuth(app);
 
 export default function Home() {
-  //Next.js router
-  const router = useRouter();
+    //Next.js router
+    const router = useRouter();
 
-  // Task 1: Implement Google Sign in with Firebase
-  // https://firebase.google.com/docs/auth/web/google-signin
-  const signIn = () => {
-    /*
+    // Task 1: Implement Google Sign in with Firebase
+    // https://firebase.google.com/docs/auth/web/google-signin
+    const signIn = () => {
+        /*
       1. Use the GoogleAuthProvider to sign in with Firebase
       2. Use signInWithRedirect to redirect the user to the Google sign in page
       3. (Optional) Use getRedirectResult to get the result of the redirect and check out what is inside :)
       4. Redirect the user to the signed-in page using Next.js router
      */
+        signInWithRedirect(auth, provider);
+        getRedirectResult(auth)
+            .then((res) => {
+                const user = res?.user;
+                console.log(user);
+                router.push("/signed-in");
+            })
+            .catch((err) => {
+                console.log("Error!");
+                console.log(err);
+            });
+    };
 
-  }
+    return (
+        <>
+            <Head>
+                <title>Sign in to see the public holidays in HK</title>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+            </Head>
+            <div className="container">
+                <main
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        flexDirection: "column",
+                    }}
+                >
+                    <h1 className="title">
+                        Welcome to{" "}
+                        <a href="https://checkmatehk.io">CheckMate</a>
+                    </h1>
+                    <h3>Sign in to see a random programming joke 😳</h3>
 
-  return (
-    <>
-      <Head>
-        <title>Sign in to see the public holidays in HK</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <div className="container">
-        <main style={{display: 'flex', justifyContent:'center', alignItems:'center', flexDirection: 'column'}}>
-          <h1 className="title">
-            Welcome to <a href="https://checkmatehk.io">CheckMate</a>
-          </h1>
-          <h3>Sign in to see a random programming joke 😳</h3>
-
-          {/* Button for user to sign in with Google */}
-          {/* Task 1: Implement Google Sign in with Firebase */}
-          <GoogleButton
-            label={'Sign in with Google'}
-            type="light"
-            style={{ width: '50%', display:"flex", justifyContent: 'center', alignItems: 'center', fontFamily: 'Roboto, sans-serif', color:'#444' }}
-            onClick={signIn}
-          />
-        </main>
-      </div>
-      </>
-  )
+                    {/* Button for user to sign in with Google */}
+                    {/* Task 1: Implement Google Sign in with Firebase */}
+                    <GoogleButton
+                        label={"Sign in with Google"}
+                        type="light"
+                        style={{
+                            width: "50%",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            fontFamily: "Roboto, sans-serif",
+                            color: "#444",
+                        }}
+                        onClick={signIn}
+                    />
+                </main>
+            </div>
+        </>
+    );
 }
