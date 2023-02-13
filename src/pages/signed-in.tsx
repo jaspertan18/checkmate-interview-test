f67f4c10e25fc387e4166a4ad70b96bca80eaa2f
jasper <sh990818@connect.hku.hk>
2023-02-13 23:45:58 +0800

    Task 2

--- src/pages/signed-in.tsx
+++ src/pages/signed-in.tsx
@@ -7,7 +7,6 @@ export default function SignedIn({
         <div>
             <h1>Signed In</h1>
             {/* Task 3: Your own presentation of the joke here (Free Style 😉 )*/}
-
             {/* End of Task 3 */}
         </div>
     );
@@ -17,10 +16,23 @@ export default function SignedIn({
 // https://official-joke-api.appspot.com/jokes/programming/random
 export const getServerSideProps: GetServerSideProps = async (context) => {
     // Fetch data from external API and pass it to the page via props.joke
+    const data = await fetch(
+        "https://official-joke-api.appspot.com/jokes/programming/random"
+    )
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data[0]);
+            return data[0];
+        })
+        .catch((err) => console.log(err));
 
     return {
         props: {
-            joke: {},
+            joke: {
+                type: data.type,
+                setup: data.setup,
+                punchline: data.punchline,
+            },
         }, // will be passed to the page component as props
     };
 };
